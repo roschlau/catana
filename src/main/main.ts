b347c304aceff04650f19971803eccf75c12533d
Robin Roschlau <robin@roschlau.me>
2025-03-07 23:54:47 +0100

    Ask for workspace folder on first start, use previously used one on subsequent starts

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,13 +1,42 @@
-import {app, BrowserWindow} from 'electron'
+import {app, BrowserWindow, dialog} from 'electron'
 import path from 'node:path'
 import started from 'electron-squirrel-startup'
+import * as fs from 'node:fs'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
   app.quit()
 }
 
-const createWindow = () => {
+async function loadDefaultWorkspacePath(): Promise<string | null> {
+  const userData = app.getPath('userData')
+  const lastWorkspacePath = path.join(userData, 'last-workspace.txt')
+  if (fs.existsSync(lastWorkspacePath)) {
+    return fs.readFileSync(lastWorkspacePath, 'utf8')
+  } else {
+    const workspaceResult = await dialog.showOpenDialog({
+      title: 'Open Workspace',
+      buttonLabel: 'Open Folder as Workspace',
+      properties: ['openDirectory', 'createDirectory', 'promptToCreate'],
+    })
+    console.log(workspaceResult)
+    if (workspaceResult.canceled) {
+      return null
+    }
+    const workspace = workspaceResult.filePaths[0]
+    fs.writeFileSync(lastWorkspacePath, workspace, 'utf8')
+    return workspace
+  }
+}
+
+const createWindow = async () => {
+  const workspacePath = await loadDefaultWorkspacePath()
+  console.log('Opening workspace', workspacePath)
+  if (!workspacePath) {
+    app.quit()
+    return
+  }
+
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -19,9 +48,9 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
+    void mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`))
+    void mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`))
   }
 
   // Open the DevTools.
@@ -46,7 +75,7 @@ app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    void createWindow()
   }
 })
 
