e45acef81a0f9f9e7c68954f58b073b4316345c8
Robin Roschlau <robin@echometer.de>
2025-05-17 11:00:49 +0200

    Remove design a bit

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -6,12 +6,13 @@ import {ThemeProvider} from 'next-themes'
 import {Provider} from 'react-redux'
 import {store} from './redux/store'
 import {ROOT_NODE} from './redux/nodesSlice'
+import {GearIcon, HomeIcon} from '@radix-ui/react-icons'
 
 const root = createRoot(document.body)
 root.render(
   <Provider store={store}>
     <ThemeProvider attribute={'class'}>
-      <Theme appearance={'inherit'}>
+      <Theme appearance={'inherit'} style={{display: 'grid'}}>
         <App/>
       </Theme>
     </ThemeProvider>
@@ -21,9 +22,12 @@ root.render(
 function App() {
   const [node, setNode] = useState(ROOT_NODE)
   return (
-    <Flex direction={'row'} p={'4'} gap={'4'}>
+    <Flex direction={'row'} p={'2'} gap={'2'} align={'stretch'} style={{background: "var(--gray-3)"}}>
       <Sidebar nodeClicked={setNode}/>
-      <Flex direction={'column'} align={'center'} flexGrow={'1'} gap={'6'}>
+      <Flex
+        direction={'column'} align={'center'} flexGrow={'1'} gap={'6'} p={'4'}
+        style={{background: "var(--gray-1)", borderRadius: "var(--radius-5)", padding: "var(--space-4)"}}
+      >
         <Heading size={'7'}>Catana</Heading>
         <Box width={'100%'} maxWidth={'600px'}>
           <NodeEditor nodeId={node}/>
@@ -38,12 +42,14 @@ function Sidebar({nodeClicked}: { nodeClicked: (nodeId: string) => void }) {
   return (
     <Flex direction={'column'} gap={'2'}>
       <Button onClick={() => nodeClicked(ROOT_NODE)}>
+        <HomeIcon/>
         Home
       </Button>
       <Button
         variant={'surface'}
         onClick={() => setShowThemePanel(!showThemePanel)}
       >
+        <GearIcon/>
         Theme
       </Button>
       {showThemePanel && <ThemePanel/>}
