b72d9c2b2f5b76286729279eff89a3b21a6ff44c
Robin Roschlau <robin@echometer.de>
2025-05-21 20:07:04 +0200

    Group nodeExpandedChanged actions per node for undo history

--- src/renderer/redux/store.ts
+++ src/renderer/redux/store.ts
@@ -1,5 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit'
-import {nodeIndexChanged, nodesSlice, titleUpdated} from './nodes/nodesSlice'
+import {nodeExpandedChanged, nodeIndexChanged, nodesSlice, titleUpdated} from './nodes/nodesSlice'
 import undoable from 'redux-undo'
 import {uiSlice} from './ui/uiSlice'
 
@@ -8,7 +8,7 @@ export const store = configureStore({
   reducer: {
     nodes: undoable(nodesSlice.reducer, {
       groupBy: (action) => {
-        const groupPerNode: string[] = [titleUpdated.type, nodeIndexChanged.type]
+        const groupPerNode: string[] = [titleUpdated.type, nodeIndexChanged.type, nodeExpandedChanged.type]
         return groupPerNode.includes(action.type)
           ? action.type + '/' + (action.payload as any).nodeId
           : null
