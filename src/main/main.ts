aea62a964c55a8f493d62b3fe9f99e54435fd49e
Robin Roschlau <robin@roschlau.me>
2025-06-29 17:11:39 +0200

    Restructure main.ts so that extension installation, window creation etc. are not run during install

--- src/main/main.ts
+++ src/main/main.ts
@@ -12,15 +12,35 @@ export const environment = MAIN_WINDOW_VITE_DEV_SERVER_URL ? 'dev' : 'prod'
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
   app.quit()
+} else {
+  if (environment === 'dev') {
+    app.whenReady().then(() => {
+      installExtension([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS])
+        .then(([redux, react]) => console.log(`Added Extensions:  ${redux.name}, ${react.name}`))
+        .catch((err) => console.log('An error occurred: ', err))
+    })
+  }
+  app.whenReady().then(createWindow)
+
+  app.on('window-all-closed', () => {
+    // Quit when all windows are closed, except on macOS. There, it's common
+    // for applications and their menu bar to stay active until the user quits
+    // explicitly with Cmd + Q.
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+  app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      void createWindow()
+    }
+  })
 }
 
-app.whenReady().then(() => {
-  installExtension([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS])
-    .then(([redux, react]) => console.log(`Added Extensions:  ${redux.name}, ${react.name}`))
-    .catch((err) => console.log('An error occurred: ', err))
-})
-
-const createWindow = async () => {
+async function createWindow() {
   session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
     callback({
       responseHeaders: {
@@ -71,28 +91,3 @@ const createWindow = async () => {
     mainWindow.webContents.openDevTools()
   }
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    void createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
